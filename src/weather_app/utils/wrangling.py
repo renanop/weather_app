93d5a89d882ce66468e6e74322ba2f7b098e1e4b
Renan Pereira <renanpereira1204@gmail.com>
2026-02-27 21:41:04 -0300

    fix: correctly apply separator argument in join_column_text utility

--- src/weather_app/utils/wrangling.py
+++ src/weather_app/utils/wrangling.py
@@ -1,11 +1,20 @@
 import pandas as pd
 
 def join_column_text(col:pd.Series, sep:str=",") -> str:
+    """ Transforms a pandas Series to a string of values separated by a separator.
+
+    Args:
+        col (pd.Series): The column to be joined.
+        sep (str, optional): The separator to use. Defaults to ",".
+
+    Returns:
+        str: A string with all values from the series separated by 'sep'.
+    """
 
     # Transform col to string type and then put it in a list
     col = col.astype(str).tolist()
 
     # Join list as a string separated by ","
-    joint_lst = ",".join(col)
+    joint_lst = sep.join(col)
 
     return joint_lst
